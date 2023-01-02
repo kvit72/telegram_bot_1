acd59d1011d9745a37aaca9b9dee36772079a3de
kvit7 <kvit72v@yandex.ru>
2023-01-02 23:00:32 +0300

    Вторая версия телеграм бота для напоминаний.

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ from aiogram import Bot, Dispatcher, executor, types
 
 
 TOKEN =
-MSG = "Программировал ли ты сегодня, {}?"
+MSG_1 = "Программировал ли ты сегодня, {}?"
+MSG_2 = "Изучал ли ты сегодня английский язык, {}?"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
@@ -17,12 +18,14 @@ async def start_handler(message: types.Message):
     user_full_name = message.from_user.full_name
     logging.info(f"{user_id=} {user_full_name=} {time.asctime()}")
     await message.reply(f"Привет, {user_full_name}!")
-
+    await bot.send_message(user_id, MSG_1.format(user_name))
+    await bot.send_message(user_id, MSG_2.format(user_name))
+"""
     for i in range(10):
         time.sleep(2)
 
         await bot.send_message(user_id, MSG.format(user_name))
-
+"""
 
 if __name__ == '__main__':
     executor.start_polling(dp)
